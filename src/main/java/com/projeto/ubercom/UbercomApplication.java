0f0a7e384a10bcaa19a00bf968c492a18c4bc320
André Castro <alpmidia@gmail.com>
2018-08-21 22:28:51 -0300

    Projeto Criado

---
+++ src/main/java/com/projeto/ubercom/UbercomApplication.java
@@ -0,0 +1,12 @@
+package com.projeto.ubercom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbercomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbercomApplication.class, args);
+	}
+}
