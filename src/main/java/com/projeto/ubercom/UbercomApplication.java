9daaef7ba39ad08eef9b14f2029c9af9dfadf4f1
André Castro <alpmidia@gmail.com>
2018-08-26 00:30:58 -0300

    Item pedido e ItemPedidoPk

--- src/main/java/com/projeto/ubercom/UbercomApplication.java
+++ src/main/java/com/projeto/ubercom/UbercomApplication.java
@@ -13,6 +13,7 @@ import com.projeto.ubercom.domain.Cidade;
 import com.projeto.ubercom.domain.Cliente;
 import com.projeto.ubercom.domain.Endereco;
 import com.projeto.ubercom.domain.Estado;
+import com.projeto.ubercom.domain.ItemPedido;
 import com.projeto.ubercom.domain.Pagamento;
 import com.projeto.ubercom.domain.PagamentoComBoleto;
 import com.projeto.ubercom.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.projeto.ubercom.repositores.CidadeRepository;
 import com.projeto.ubercom.repositores.ClienteRepository;
 import com.projeto.ubercom.repositores.EnderecoRepository;
 import com.projeto.ubercom.repositores.EstadoRepository;
+import com.projeto.ubercom.repositores.ItemPedidoRepository;
 import com.projeto.ubercom.repositores.PagamentoRepository;
 import com.projeto.ubercom.repositores.PedidoRepository;
 import com.projeto.ubercom.repositores.ProdutoRepository;
@@ -49,12 +51,15 @@ public class UbercomApplication implements CommandLineRunner {
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UbercomApplication.class, args);
@@ -93,33 +98,46 @@ public class UbercomApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-	       Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36372213769865", TipoCliente.PESSOAFISICA);
-	       cli1.getTelefones().addAll(Arrays.asList("1234234","4234234"));
-	       
-	       Endereco e1 = new Endereco(null, "Rua Flores","300","Apart 303","Jardim","38220834",cli1,c1);
-	       Endereco e2 = new Endereco(null, "Av Matos","105","Sala 800","Centro","38777012",cli1,c2);
-	       
-	       cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
-	       
-	       clienteRepository.saveAll(Arrays.asList(cli1));
-	       enderecoRepository.saveAll(Arrays.asList(e1,e2));
-	       
-	       SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-			
-			Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 11:00"), cli1, e1);
-			Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 20:00"), cli1, e2);
-			
-			Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
-			ped1.setPagamento(pagto1);
-			
-			Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 12:12"), null);
-			ped2.setPagamento(pagto2);
-			
-			cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-					
-			pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
-			pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));	       
-	       
-		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36372213769865", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("1234234", "4234234"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apart 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Av Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 11:00"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 20:00"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 12:12"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+
 	}
 }
