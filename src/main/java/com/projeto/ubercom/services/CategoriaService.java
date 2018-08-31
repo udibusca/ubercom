900509b3fb0fbed68be64cf7d21b5c8928c72a96
Andre Castro <alpmidia@gmail.com>
2018-08-30 22:05:41 -0300

    Listando todas categorias com DTO

--- src/main/java/com/projeto/ubercom/services/CategoriaService.java
+++ src/main/java/com/projeto/ubercom/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.projeto.ubercom.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +18,29 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
+	/**
+	 * @param id
+	 * @return
+	 */
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
+	/**
+	 * @param obj
+	 * @return
+	 */
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
 
+	/**
+	 * @param obj
+	 * @return
+	 */
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
@@ -44,6 +57,13 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	/**
+	 * @return
+	 */
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 	
 	
