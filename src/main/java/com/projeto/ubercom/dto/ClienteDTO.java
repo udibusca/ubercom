f3ec876affda5205dec10bde15c803b1c59b98de
André Castro <alpmidia@gmail.com>
2018-09-06 21:43:05 -0300

    Validação de email não repetido custumizado na atualizacao

--- src/main/java/com/projeto/ubercom/dto/ClienteDTO.java
+++ src/main/java/com/projeto/ubercom/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.projeto.ubercom.domain.Cliente;
+import com.projeto.ubercom.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
