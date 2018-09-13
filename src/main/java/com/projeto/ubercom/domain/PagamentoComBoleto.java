2a9050e66302fe3639d5d9fbfaa0f3d0420774ff
André Castro <alpmidia@gmail.com>
2018-09-13 20:13:31 -0300

    Inserindo Pedido

--- src/main/java/com/projeto/ubercom/domain/PagamentoComBoleto.java
+++ src/main/java/com/projeto/ubercom/domain/PagamentoComBoleto.java
@@ -13,10 +13,10 @@ import com.projeto.ubercom.domain.enums.EstadoPagamento;
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
-	@JsonFormat(pattern="dd/MM/yyyy")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
 
-	@JsonFormat(pattern="dd/MM/yyyy")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataPagamento;
 
 	public PagamentoComBoleto() {
--- src/main/java/com/projeto/ubercom/services/PedidoService.java
+++ src/main/java/com/projeto/ubercom/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.projeto.ubercom.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projeto.ubercom.domain.ItemPedido;
+import com.projeto.ubercom.domain.PagamentoComBoleto;
 import com.projeto.ubercom.domain.Pedido;
+import com.projeto.ubercom.domain.enums.EstadoPagamento;
+import com.projeto.ubercom.repositores.ItemPedidoRepository;
+import com.projeto.ubercom.repositores.PagamentoRepository;
 import com.projeto.ubercom.repositores.PedidoRepository;
 import com.projeto.ubercom.services.exceptions.ObjectNotFoundException;
 
@@ -15,9 +21,49 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	/**
+	 * @param id
+	 * @return
+	 */
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id Pedido : " + id + ", Tipo: " + Pedido.class.getName()));
 	}
+	
+	/**
+	 * @param obj
+	 * @return
+	 */
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+	}	
 }
