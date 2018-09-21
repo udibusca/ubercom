15335d93433cac25cb04d992cd10bd10c7edb90c
André Castro <alpmidia@gmail.com>
2018-09-20 21:51:38 -0300

    Restricao por conteudo: cliente so recupera seus pedido

--- src/main/java/com/projeto/ubercom/repositores/PedidoRepository.java
+++ src/main/java/com/projeto/ubercom/repositores/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.projeto.ubercom.repositores;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.projeto.ubercom.domain.Cliente;
 import com.projeto.ubercom.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
+	
 }
--- src/main/java/com/projeto/ubercom/services/PedidoService.java
+++ src/main/java/com/projeto/ubercom/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.projeto.ubercom.domain.Cliente;
 import com.projeto.ubercom.domain.ItemPedido;
 import com.projeto.ubercom.domain.PagamentoComBoleto;
 import com.projeto.ubercom.domain.Pedido;
@@ -13,6 +17,8 @@ import com.projeto.ubercom.domain.enums.EstadoPagamento;
 import com.projeto.ubercom.repositores.ItemPedidoRepository;
 import com.projeto.ubercom.repositores.PagamentoRepository;
 import com.projeto.ubercom.repositores.PedidoRepository;
+import com.projeto.ubercom.security.UserSS;
+import com.projeto.ubercom.services.exceptions.AuthorizationException;
 import com.projeto.ubercom.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,6 +38,9 @@ public class PedidoService {
 	
 	@Autowired
 	private ProdutoService produtoService;
+	
+	@Autowired
+	private ClienteService clienteService;
 	
 	/**
 	 * @param id
@@ -65,5 +74,15 @@ public class PedidoService {
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
 		return obj;
-	}	
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 }
