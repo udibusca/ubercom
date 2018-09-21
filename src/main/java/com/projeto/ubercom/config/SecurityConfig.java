f8c2488497cf4108821324df9740959dcbf15817
André Castro <alpmidia@gmail.com>
2018-09-20 21:11:21 -0300

    Implementando autorizacao

--- src/main/java/com/projeto/ubercom/config/SecurityConfig.java
+++ src/main/java/com/projeto/ubercom/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.projeto.ubercom.security.JWTAuthenticationFilter;
+import com.projeto.ubercom.security.JWTAuthorizationFilter;
 import com.projeto.ubercom.security.JWTUtil;
 
 @Configuration
@@ -54,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
  	
